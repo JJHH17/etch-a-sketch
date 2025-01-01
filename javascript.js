6a6713ce6bcc848a16be175650ebf8821043f8ed
James Hatfield <james.hatfield17@icloud.com>
2025-01-01 11:41:53 +0000

    Add event handler for button click and append grid
    - Button click event handler added
    - Loop that appends new grid to page added
    - Prompt for number added
    - Added psuedocode to revise grid formula

--- javascript.js
+++ javascript.js
@@ -35,5 +35,24 @@ resetButton.addEventListener("click", () => {
     divArray = [];
 
     // Allow the user to enter a value which will append the new grid
-    
+    // accept 1:100 whole numbers
+    let numberEntry = prompt('Please enter a value for your new grid! (1-100): ');
+
+    // Error handling strings
+
+    // Error handling decimals 
+
+    // loop creates new grid based on input 
+    for (let n = 0; n < numberEntry; n++) {
+        const newContent = document.createElement("div");
+        newContent.style.width = "calc(100% / 16)";
+        newContent.style.height = "calc(100% / 16)";
+        newContent.style.border = "1px solid black";
+        // Appended to previously emptied container div and array
+        container.appendChild(newContent);
+        divArray.push(newContent);
+    }
+
 })
+
+// We need to revise how we handle/form the grid as this is currently broken upon a new grid being formed, revise the formula that's run when the user enters a new grid value
